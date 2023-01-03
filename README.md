9e2f1112c159c2cd0df6a33ba1aef45e661c06cf
Kenneth Wilke <kenneth.wilke@gmail.com>
2023-01-02 20:36:17 -0600

    zOS initial

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 These are my scripts for [BitBurner](https://danielyxie.github.io/bitburner/)
 
 Quality varies!
+
+# zOS
+
+This is (will be) my over-engineered co-pilot for Bitburner :D
+
+```
+wget https://raw.githubusercontent.com/KennethWilke/z-bitburner/main/zOS.js zOS.js
+run zOS.js --tail
+```
+
--- zOS.js
+++ zOS.js
@@ -1,16 +1,4 @@
 /** @param {NS} ns */
 export async function main(ns) {
-	ns.clearLog();
-	ns.disableLog("disableLog");
-	ns.disableLog("getServerMaxMoney");
-	let localhost = ns.getHostname();
-	let money_max = ns.getServerMaxMoney(localhost);
-	
-	while (true) {
-		await ns.hack(localhost);
-		await ns.grow(localhost);
-		await ns.weaken(localhost);
-		await ns.sleep(1000);
-		ns.clearLog();
-	}
+	ns.print("hey!");
 }
